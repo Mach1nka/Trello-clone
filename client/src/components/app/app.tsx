a756ae0687cc5516d20c513954a8794d8e7c740c
Artem Uspenski <uspenskiyart@gmail.com>
2021-04-11 18:35:28 +0300

    feat: add commands for building and starting, create webpack config

---
+++ client/src/components/app/app.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = (): JSX.Element => <h1>hello</h1>;
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".svg"],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(
+            {
+                minify: {
+                    collapseWhitespace: true,
+                    removeComments: true,
+                },
+                template: path.join(__dirname, "src", "index.html")
+            })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|ts|tsx)$/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader", 
+                    "sass-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: { plugins: [ require("autoprefixer") ] }
+                        },
+                    },
+                ]
+            }
+        ]
+    },
+    devServer: {
+        overlay: true,
+        contentBase: path.join(__dirname, "dist"),
+        port: 8080,
+        hot: true,
+    }
+};
