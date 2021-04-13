80ee1f29604b8ed914cfef8d41bde6e490b50807
Artem Uspenski <uspenskiyart@gmail.com>
2021-04-13 16:46:39 +0300

    feat: add register controller

--- server/app/controllers/auth.ts
+++ server/app/controllers/auth.ts
@@ -1,23 +1,25 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
 import User from '../models/user';
 
 const register = async (req: Request, res: Response): Promise<void> => {
   const { login, password }: { login: string; password: string } = req.body;
   const candidate: boolean = await User.findOne({ login });
   if (!candidate) {
+    const salt = bcrypt.genSaltSync(10);
     const user = new User({
       login,
-      password
+      password: bcrypt.hashSync(password, salt)
     });
     try {
-      await user.save(user);
+      await user.save();
       res.status(201).end();
     } catch (error) {
       console.log(error);
-      res.status(400).end();
+      res.status(500).end();
     }
   } else {
-    res.status(400).json({ message: 'user has been registered' });
+    res.status(409).json({ message: 'user has been registered' });
   }
 };
 
