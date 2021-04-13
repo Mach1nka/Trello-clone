a06923712905bdb85eeb81ba5fb30b59919760a2
Artem Uspenski <uspenskiyart@gmail.com>
2021-04-13 17:42:03 +0300

    fix: add interface for user

--- server/app/controllers/auth.ts
+++ server/app/controllers/auth.ts
@@ -1,10 +1,18 @@
+import mongoose from 'mongoose';
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import User from '../models/user';
 
+interface Candidate {
+  _id: mongoose.Types.ObjectId;
+  login: string;
+  password: string;
+  __v: number;
+}
+
 const register = async (req: Request, res: Response): Promise<void> => {
   const { login, password }: { login: string; password: string } = req.body;
-  const candidate: boolean = await User.findOne({ login });
+  const candidate: Candidate | null = await User.findOne({ login });
   if (!candidate) {
     const salt = bcrypt.genSaltSync(10);
     const user = new User({
