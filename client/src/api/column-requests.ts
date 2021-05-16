b0ea18f979cc0de51cdee76c9400dd39b31eb39e
Artem Uspenski <uspenskiyart@gmail.com>
2021-05-16 22:54:07 +0300

    fix: edit column requests

--- client/src/api/column-requests.ts
+++ client/src/api/column-requests.ts
@@ -5,74 +5,54 @@ import {
   DataForUpdatingColumnPos,
   DataForDeletingColumn
 } from '../store/column/actions';
-import getToken from '../../utils/get-token';
+import { requestHeader, responseHandler } from './constants';
 
 function getColumns(boardId: string): Promise<Response> {
-  const authToken = getToken();
   return fetch(`${serverURL}/columns/${boardId}`, {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: authToken
-    }
+    headers: requestHeader()
   })
-    .then((resp) => (resp.status === 401 ? resp.status : resp.json()))
+    .then((resp) => responseHandler(resp))
     .catch((error) => error);
 }
 
 function createColumn(data: DataForCreatingColumn): Promise<Response> {
-  const authToken = getToken();
   return fetch(`${serverURL}/column`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: authToken
-    },
+    headers: requestHeader(),
     body: JSON.stringify(data)
   })
-    .then((resp) => (resp.status === 401 ? resp.status : resp.json()))
+    .then((resp) => responseHandler(resp))
     .catch((error) => error);
 }
 
 function updateColumnName(data: DataForRenamingColumn): Promise<Response> {
-  const authToken = getToken();
   return fetch(`${serverURL}/column/name`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: authToken
-    },
+    headers: requestHeader(),
     body: JSON.stringify(data)
   })
-    .then((resp) => (resp.status === 401 ? resp.status : resp.json()))
+    .then((resp) => responseHandler(resp))
     .catch((error) => error);
 }
 
 function updateColumnPosition(data: DataForUpdatingColumnPos): Promise<Response> {
-  const authToken = getToken();
   return fetch(`${serverURL}/column/position`, {
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: authToken
-    },
+    headers: requestHeader(),
     body: JSON.stringify(data)
   })
-    .then((resp) => (resp.status === 401 ? resp.status : resp.json()))
+    .then((resp) => responseHandler(resp))
     .catch((error) => error);
 }
 
 function deleteColumn(data: DataForDeletingColumn): Promise<Response> {
-  const authToken = getToken();
   return fetch(`${serverURL}/column`, {
     method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-      Authorization: authToken
-    },
+    headers: requestHeader(),
     body: JSON.stringify(data)
   })
-    .then((resp) => (resp.status === 401 ? resp.status : resp.json()))
+    .then((resp) => responseHandler(resp))
     .catch((error) => error);
 }
 
