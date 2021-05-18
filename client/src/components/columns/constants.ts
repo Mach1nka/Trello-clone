29137e64c6c8590e104f34552a1c4d6e1e246d1c
Artem Uspenski <uspenskiyart@gmail.com>
2021-05-19 00:43:48 +0300

    fix: fix column name styles

--- client/src/components/columns/constants.ts
+++ client/src/components/columns/constants.ts
@@ -3,7 +3,9 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles(() => ({
   columnName: {
     fontSize: '19px',
-    fontWeight: 400
+    fontWeight: 400,
+    maxWidth: '190px',
+    overflowWrap: 'break-word'
   }
 }));
 
