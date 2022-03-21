f88f54840308f4bdb57d623ba278cefe79c8c4a5
Mach1nka <uspenskiyart@gmail.com>
2022-03-22 02:14:59 +0300

    feat: migrate user service on sql

--- server/app/services/users.ts
+++ server/app/services/users.ts
@@ -1,24 +1,15 @@
-import User, { UserInDB } from '../models/user';
+import { userRepository } from '../database/repositories';
+import { User } from '../entities/user';
 
-interface SearchUser {
-  id: string;
-  login: string;
-}
+async function getUsersService(searchedValue: string, userId: string): Promise<User[]> {
+  const users: User[] = await userRepository()
+    .createQueryBuilder('users')
+    .where('users.login ILIKE :searchValue', { searchValue: `%${searchedValue}%` })
+    .andWhere('users.id != :userId', { userId })
+    .select(['users.login', 'users.id'])
+    .getMany();
 
-async function getUsersService(searchValue: string, userId: string): Promise<SearchUser[]> {
-  const regex = new RegExp(searchValue);
-  const allUsers: UserInDB[] = await User.find({ login: { $regex: regex, $options: 'i' } });
-
-  const filteredUsers: UserInDB[] = allUsers.filter(
-    (el) => el._id.toString() !== userId.toString()
-  );
-
-  const preparedData: SearchUser[] = filteredUsers.map((el) => ({
-    id: String(el._id),
-    login: el.login
-  }));
-
-  return preparedData;
+  return users;
 }
 
 export default getUsersService;
